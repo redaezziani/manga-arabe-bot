712fbd942e9583e4d9ecb6f4a2bca1c4efc0f646
klaus <118160573+redaezziani@users.noreply.github.com>
2023-11-06 19:00:19 +0100

    add

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ import getManga from'./utils/getManga.js';
 import removeTrash from'./utils/removeTrash.js';
 
 const bot = new Telegraf(process.env.MANGA_TELEGRAM_SECRET_KEY);
-
 bot.start((ctx) => {
     ctx.reply('Welcome to Manga Bot. To get a manga, send /getmanga');
 });
@@ -23,19 +22,15 @@ bot.on('text', async (ctx) => {
         ctx.reply('Please enter the manga name and chapter number in the correct format.');
         return;
     }
-
     const mangaName = input[0];
     const mangaChapter = input[1];
     await getManga(mangaName, mangaChapter);
-
     setTimeout(() => {
         const pdfPath = `${mangaName}-${mangaChapter}.pdf`;
-
     if (fs.existsSync(pdfPath)) {
          ctx.replyWithDocument({
             source: pdfPath
         });
-
         console.log('Reply sent.');
          ctx.reply('Thanks for using Manga Bot');
          ctx.reply('Send /getmanga to get another manga');
